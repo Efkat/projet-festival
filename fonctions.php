06b9d5d487549a03bd0e5a83162eb0806263b009
Efkat <hugo.benabdelhak>
2020-10-31 15:52:55 +0100

    Ajout de  différentes routes (index/profil/register/liste)

--- fonctions.php
+++ fonctions.php
@@ -1,1 +1,34 @@
 <?php
+Flight::register('db', 'PDO', array('mysql:host=127.0.0.1;dbname=festival','root','root'),
+    function ($db){
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+);
+
+/**
+ * Name = "home"
+ */
+Flight::route('/', function (){
+    Flight::render('templates/index.tpl',array(null));
+});
+
+/**
+ * Name = "liste"
+ */
+Flight::route('/liste', function (){
+    Flight::render('templates/liste.tpl', array(null));
+});
+
+/**
+ * Name = "profil"
+ */
+Flight::route("/profil", function (){
+    Flight::render('templates/profil.tpl', array(null));
+});
+
+/**
+ * Name = "register"
+ */
+Flight::route("/register", function (){
+    Flight::render('templates/register.tpl', array(null));
+});
--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+require 'vendor/autoload.php';
+
+include 'fonctions.php';
+
+Flight::register('view', 'Smarty', array(), function ($smarty){
+    $smarty->template_dir = './templates/';
+    $smarty->compile_dir='./templates_c/';
+    $smarty->config_dir='./config/';
+    $smarty->cache_dir='./cache/';
+});
+
+Flight::map('render', function(string $template, array $data){
+    Flight::view()->assign($data);
+    Flight::view()->display($template);
+});
+
+Flight::start();
