1313ffdecf927383f0ecf257cdc58f2a1bf9130b
NinoLS <belicnino77@gmail.com>
2020-11-03 10:06:00 +0100

    templates compilés

--- templates_c/9b3107d55290c37f426799e931a5284866d36768_0.file.login.tpl.php
+++ templates_c/9b3107d55290c37f426799e931a5284866d36768_0.file.login.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.36, created on 2020-11-02 17:29:09
+/* Smarty version 3.1.36, created on 2020-11-03 08:51:59
   from 'C:\laragon\www\projet-festival\templates\login.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_5fa041e5034753_37973771',
+  'unifunc' => 'content_5fa11a2f8e18f4_14227154',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '9b3107d55290c37f426799e931a5284866d36768' => 
     array (
       0 => 'C:\\laragon\\www\\projet-festival\\templates\\login.tpl',
-      1 => 1604338148,
+      1 => 1604393079,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5fa041e5034753_37973771 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5fa11a2f8e18f4_14227154 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html>
     <head>
@@ -29,12 +29,12 @@ function content_5fa041e5034753_37973771 (Smarty_Internal_Template $_smarty_tpl)
         <link rel="stylesheet" type="text/css" href="./styles/style.css">
         <title>Login</title>
     </head>
-    <main>
+    <body>
         <header>
             
 
         </header>
-        <body>
+        <main>
             <h1>Se Connecter</h1>
             <section>
                 <form action="./login" method="POST">
@@ -54,13 +54,13 @@ function content_5fa041e5034753_37973771 (Smarty_Internal_Template $_smarty_tpl)
 </p>
 
             </section>
-        </body>
+        </main>
         <footer>
             <p>Nino BELIC & Hugo BENAB DEL K</p>
 
         </footer>
 
-    </main>
+    </body>
 
 
 
---
+++ templates_c/a0e3c2a952ef01f302b1c5b98b416197788d6159_0.file.success.tpl.php
@@ -0,0 +1,50 @@
+<?php
+/* Smarty version 3.1.36, created on 2020-11-03 08:51:57
+  from 'C:\laragon\www\projet-festival\templates\success.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.36',
+  'unifunc' => 'content_5fa11a2decfcf2_09628762',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a0e3c2a952ef01f302b1c5b98b416197788d6159' => 
+    array (
+      0 => 'C:\\laragon\\www\\projet-festival\\templates\\success.tpl',
+      1 => 1604393043,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5fa11a2decfcf2_09628762 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="./styles/style.css">
+        <title>Success</title>
+    </head>
+    <body>
+        <header>
+
+
+        </header> 
+        <main>
+            <h3>Enregistré!</h3>
+            <p>Données transférées à la base!</p>
+            <a href="./login">Connexion</a>
+        </main>   
+        <footer>
+
+            
+        </footer>
+
+    </body>
+
+</html><?php }
+}
