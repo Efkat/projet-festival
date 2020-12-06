f46d36611f0d1cc6e1940fd33e09c105f5e22d61
NinoLS <belicnino77@gmail.com>
2020-12-06 10:39:47 +0100

    Modif : script gestionMembres.js simplified

--- script/gestionMembres.js
+++ script/gestionMembres.js
@@ -1,16 +1,30 @@
-const membersMax = 8;
 let membersCount = 1
+let membersInformations = ""
+let membersDisableInput = document.getElementById('membresVariables')
+
+const membersMax = 8;
 const addButton = document.getElementById("add")
 const delButton = document.getElementById("delete")
 const cardContainer = document.getElementById("container")
 const cards = document.getElementsByClassName("card")
-let membersInformations = ""
-let membersDisableInput = document.getElementById('membresVariables')
 
-//Event listener on add button
+
+/* BUTTONS */
+//Add button
 addButton.addEventListener("click", function (event) {
     event.preventDefault();
-    if (membersCount < membersMax) {
+    if (membersCount < membersMax /*&& [verification non vide] */) {
+        
+        //CONFIRMATION
+        let inputs = cards[membersCount-1].querySelectorAll('input')
+        for (let j = 0; j < inputs.length; j++) {
+            inputs[j].setAttribute('disabled', "")
+        }
+        if(membersCount>1){ membersInformations+="\\" }
+        membersInformations += getCardInformation(cards[membersCount-1])
+        membersDisableInput.setAttribute("value", membersInformations)
+        
+        //ADD
         membersCount++
         cardContainer.insertAdjacentHTML('beforeend', '<section class="card border-secondary m-2" style="width: 12rem">\n' +
             '                                    <div class="card-header">MEMBRE 1</div>\n' +
@@ -24,26 +38,17 @@ addButton.addEventListener("click", function (event) {
             '                                        <button type="button" class="btn btn-sm btn-outline-info">Confirmer membre</button>\n' +
             '                                    </div>\n' +
             '                                </section>')
+        alert(membersInformations)
         updateCardsNumber()
+        
     } else {
         alert("Nombre de membres maximale atteint")
     }
+    
+    
 });
 
-//Member confirmation
-addButton.addEventListener('click', function (event){
-    event.preventDefault();
-    cardContainer.lastElementChild.querySelector('.btn-sm').addEventListener('click', function (){
-        membersInformations += "\\" + getCardInformation(cardContainer.lastElementChild)
-        membersDisableInput.setAttribute("value", membersInformations)
-        let inputs = cardContainer.lastElementChild.querySelectorAll('input')
-        for (let j = 0; j < inputs.length; j++) {
-            inputs[j].setAttribute('disabled', "")
-        }
-    })
-})
-
-//Event listener on delete button
+//Delete Button
 delButton.addEventListener("click", function (event) {
     event.preventDefault();
     if (membersCount > 1) {
@@ -55,16 +60,9 @@ delButton.addEventListener("click", function (event) {
     }
 })
 
-//Event listener for card 0 confirmation
-cards[0].querySelector('.btn-sm').addEventListener('click', function () {
-    let inputs = cards[0].querySelectorAll('input')
-    for (let j = 0; j < inputs.length; j++) {
-        inputs[j].setAttribute('disabled', "")
-    }
-    membersInformations += getCardInformation(cards[0])
-    membersDisableInput.setAttribute("value", membersInformations)
-})
 
+
+/* FUNCTIONS */
 function getCardInformation(card) {
     let nom = card.querySelector(".nom-input").value
     let prenom = card.querySelector(".prenom-input").value
