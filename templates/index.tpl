0e8fced490ef6b6c772ac20832fb37326a0fa06d
NinoLS <belicnino77@gmail.com>
2020-12-06 18:46:59 +0100

    Modif : style on main buttons in index.tpl

--- templates/index.tpl
+++ templates/index.tpl
@@ -6,30 +6,32 @@
     <main>
         <div class="jumbotron w-75 mx-auto rounded m-5">
             <h2 class="text-center m-3 display-3">Bienvenue {if $name != null}{$name}{else}visiteur{/if} sur la page d'inscription !</h2>
-            {if $name==null}
-                {*Si déconnecté*}
-                <a href="./register">S'enregistrer</a>
-                <a href="./login">Se Connecter</a>
-            {else}
-                {*Si connecté*}
-                {if $candidature==0 && $name!=admin}
-                    {*Si pas de candidature et si ce n'est pas l'admin*}
-                    <a href="./candidature">Candidature</a>
+            <div class="card w-50" style="margin: 0 auto;">           
+                {if $name==null}
+                    {*Si déconnecté*}
+                    <a class="btn btn-primary btn-lg mt-2" href="./register">S'enregistrer</a>
+                    <a class="btn btn-primary btn-lg mt-2" href="./login">Se Connecter</a>
                 {else}
-                    {*Si admin*}
-                    {if $name==admin}
-                        <a href="./liste">Liste Candidatures</a>
+                    {*Si connecté*}
+                    {if $candidature==0 && $name!=admin}
+                        {*Si pas de candidature et si ce n'est pas l'admin*}
+                        <a class="btn btn-primary btn-lg " href="./candidature">Candidature</a>
                     {else}
-                        {*Si déjà candidature*}
-                        <a href="./c_consulter">Consulter Candidature</a>
-                        <a href="./c_edit">Editer Candidature</a>
+                        {*Si admin*}
+                        {if $name==admin}
+                            <a class="btn btn-primary btn-lg " href="./liste">Liste Candidatures</a>
+                        {else}
+                            {*Si déjà candidature*}
+                            <a class="btn btn-primary btn-lg mt-2" href="./c_consulter">Consulter Candidature</a>
+                            <a class="btn btn-primary btn-lg mt-2" href="./c_edit">Editer Candidature</a>
+                        {/if}
                     {/if}
+                    <hr class="my-2">
+                    <p class="lead text-center">
+                        <a class="btn btn-primary btn-lg " href="./logout" role="button">Déconnexion</a>
+                    </p>
                 {/if}
-                <hr class="my-2">
-                <p class="lead text-center">
-                    <a class="btn btn-primary btn-lg " href="./logout" role="button">Déconnexion</a>
-                </p>
-            {/if}
+            </div>
 
         </div>
         <p>{$erreurs}</p>
--- templates_c/e5a054b433839de6d58483ebbb308a70c349d65c_0.file.index.tpl.php
+++ templates_c/e5a054b433839de6d58483ebbb308a70c349d65c_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.36, created on 2020-12-06 12:34:33
+/* Smarty version 3.1.36, created on 2020-12-06 17:46:04
   from 'C:\laragon\www\projet-festival\templates\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_5fcccfd94f5a34_33845628',
+  'unifunc' => 'content_5fcd18dc83e437_12439588',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e5a054b433839de6d58483ebbb308a70c349d65c' => 
     array (
       0 => 'C:\\laragon\\www\\projet-festival\\templates\\index.tpl',
-      1 => 1607258069,
+      1 => 1607276761,
       2 => 'file',
     ),
   ),
@@ -20,26 +20,26 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5fcccfd94f5a34_33845628 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5fcd18dc83e437_12439588 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_20607021605fcccfd94e8867_43660312', "title");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_903390605fcd18dc830bb4_30302930', "title");
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_5791585665fcccfd94f0b36_64914696', "content");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_14545145505fcd18dc838c44_49374408', "content");
 $_smarty_tpl->inheritance->endChild($_smarty_tpl, "base.tpl");
 }
 /* {block "title"} */
-class Block_20607021605fcccfd94e8867_43660312 extends Smarty_Internal_Block
+class Block_903390605fcd18dc830bb4_30302930 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'title' => 
   array (
-    0 => 'Block_20607021605fcccfd94e8867_43660312',
+    0 => 'Block_903390605fcd18dc830bb4_30302930',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -51,12 +51,12 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 }
 /* {/block "title"} */
 /* {block "content"} */
-class Block_5791585665fcccfd94f0b36_64914696 extends Smarty_Internal_Block
+class Block_14545145505fcd18dc838c44_49374408 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'content' => 
   array (
-    0 => 'Block_5791585665fcccfd94f0b36_64914696',
+    0 => 'Block_14545145505fcd18dc838c44_49374408',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -67,25 +67,27 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
             <h2 class="text-center m-3 display-3">Bienvenue <?php if ($_smarty_tpl->tpl_vars['name']->value != null) {
 echo $_smarty_tpl->tpl_vars['name']->value;
 } else { ?>visiteur<?php }?> sur la page d'inscription !</h2>
-            <?php if ($_smarty_tpl->tpl_vars['name']->value == null) {?>
-                                <a href="./register">S'enregistrer</a>
-                <a href="./login">Se Connecter</a>
-            <?php } else { ?>
-                                <?php if ($_smarty_tpl->tpl_vars['candidature']->value == 0 && $_smarty_tpl->tpl_vars['name']->value != 'admin') {?>
-                                        <a href="./candidature">Candidature</a>
+            <div class="card w-50" style="margin: 0 auto;">           
+                <?php if ($_smarty_tpl->tpl_vars['name']->value == null) {?>
+                                        <a class="btn btn-primary btn-lg mt-2" href="./register">S'enregistrer</a>
+                    <a class="btn btn-primary btn-lg mt-2" href="./login">Se Connecter</a>
                 <?php } else { ?>
-                                        <?php if ($_smarty_tpl->tpl_vars['name']->value == 'admin') {?>
-                        <a href="./liste">Liste Candidatures</a>
+                                        <?php if ($_smarty_tpl->tpl_vars['candidature']->value == 0 && $_smarty_tpl->tpl_vars['name']->value != 'admin') {?>
+                                                <a class="btn btn-primary btn-lg " href="./candidature">Candidature</a>
                     <?php } else { ?>
-                                                <a href="./c_consulter">Consulter Candidature</a>
-                        <a href="./c_edit">Editer Candidature</a>
+                                                <?php if ($_smarty_tpl->tpl_vars['name']->value == 'admin') {?>
+                            <a class="btn btn-primary btn-lg " href="./liste">Liste Candidatures</a>
+                        <?php } else { ?>
+                                                        <a class="btn btn-primary btn-lg mt-2" href="./c_consulter">Consulter Candidature</a>
+                            <a class="btn btn-primary btn-lg mt-2" href="./c_edit">Editer Candidature</a>
+                        <?php }?>
                     <?php }?>
+                    <hr class="my-2">
+                    <p class="lead text-center">
+                        <a class="btn btn-primary btn-lg " href="./logout" role="button">Déconnexion</a>
+                    </p>
                 <?php }?>
-                <hr class="my-2">
-                <p class="lead text-center">
-                    <a class="btn btn-primary btn-lg " href="./logout" role="button">Déconnexion</a>
-                </p>
-            <?php }?>
+            </div>
 
         </div>
         <p><?php echo $_smarty_tpl->tpl_vars['erreurs']->value;?>
