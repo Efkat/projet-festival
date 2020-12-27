3f7b397b6ba7aaed6ce3e34b3499b348a887e0f1
NinoLS <belicnino77@gmail.com>
2020-12-27 23:33:02 +0100

    Add : style on liste (admin)

--- templates/liste.tpl
+++ templates/liste.tpl
@@ -7,12 +7,22 @@
             <h1>Liste</h1>
             <section>
                 <table>
+                    <tr class="border-bottom border-primary font-italic">
+                        <td></td>
+                        <td class="text-info font-weight-bold"><u>Nom Groupe</u></td> {*nom groupe*}
+                        <td><u>Département</u></td>
+                        <td><u>Scène</u></td> {*scene*}
+                        <td><u>Style</u></td> {*style*}
+                        <td><u>Année Création</u></td> 
+                        <td><u>Présentation</u></td>
+                        <td><u>Expérience</u></td> 
+                    </tr>
+
                     {foreach from=$lignes item=$ligne}
                         <tr>
                             {*href= /liste/details/nom_groupe*}
                             <td><a href="./details/{$ligne['nom_groupe']}">Détails</a></td>
-
-                            <td>{$ligne['nom_groupe']}</td> {*nom groupe*}
+                            <td class="text-info font-weight-bold">{$ligne['nom_groupe']}</td> {*nom groupe*}
                             <td>{$ligne['departement']}</td>
                             <td>{$ligne['nom_type']}</td> {*scene*}
                             <td>{$ligne['nom_style']}</td> {*style*}
--- templates_c/b2ad4fadb7f498fef35e37593b3333bc92c1620d_0.file.liste.tpl.php
+++ templates_c/b2ad4fadb7f498fef35e37593b3333bc92c1620d_0.file.liste.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.36, created on 2020-12-06 17:54:16
+/* Smarty version 3.1.36, created on 2020-12-27 22:30:34
   from 'C:\laragon\www\projet-festival\templates\liste.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_5fcd1ac8194372_60703684',
+  'unifunc' => 'content_5fe90b0a34b967_32011060',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'b2ad4fadb7f498fef35e37593b3333bc92c1620d' => 
     array (
       0 => 'C:\\laragon\\www\\projet-festival\\templates\\liste.tpl',
-      1 => 1607277254,
+      1 => 1609108233,
       2 => 'file',
     ),
   ),
@@ -20,26 +20,26 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5fcd1ac8194372_60703684 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5fe90b0a34b967_32011060 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_7603956885fcd1ac817d922_56828824', "title");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_2725506335fe90b0a33af56_63190971', "title");
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_852195445fcd1ac817e558_00304131', "content");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_18551600775fe90b0a33c118_76925377', "content");
 $_smarty_tpl->inheritance->endChild($_smarty_tpl, "base.tpl");
 }
 /* {block "title"} */
-class Block_7603956885fcd1ac817d922_56828824 extends Smarty_Internal_Block
+class Block_2725506335fe90b0a33af56_63190971 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'title' => 
   array (
-    0 => 'Block_7603956885fcd1ac817d922_56828824',
+    0 => 'Block_2725506335fe90b0a33af56_63190971',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -51,12 +51,12 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 }
 /* {/block "title"} */
 /* {block "content"} */
-class Block_852195445fcd1ac817e558_00304131 extends Smarty_Internal_Block
+class Block_18551600775fe90b0a33c118_76925377 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'content' => 
   array (
-    0 => 'Block_852195445fcd1ac817e558_00304131',
+    0 => 'Block_18551600775fe90b0a33c118_76925377',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -66,6 +66,14 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
             <h1>Liste</h1>
             <section>
                 <table>
+                    <tr class="border-bottom border-primary font-italic">
+                        <td></td>
+                        <td class="text-info font-weight-bold"><u>Nom Groupe</u></td>                         <td><u>Département</u></td>
+                        <td><u>Scène</u></td>                         <td><u>Style</u></td>                         <td><u>Année Création</u></td> 
+                        <td><u>Présentation</u></td>
+                        <td><u>Expérience</u></td> 
+                    </tr>
+
                     <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['lignes']->value, 'ligne');
 $_smarty_tpl->tpl_vars['ligne']->do_else = true;
@@ -75,8 +83,7 @@ $_smarty_tpl->tpl_vars['ligne']->do_else = false;
                         <tr>
                                                         <td><a href="./details/<?php echo $_smarty_tpl->tpl_vars['ligne']->value['nom_groupe'];?>
 ">Détails</a></td>
-
-                            <td><?php echo $_smarty_tpl->tpl_vars['ligne']->value['nom_groupe'];?>
+                            <td class="text-info font-weight-bold"><?php echo $_smarty_tpl->tpl_vars['ligne']->value['nom_groupe'];?>
 </td>                             <td><?php echo $_smarty_tpl->tpl_vars['ligne']->value['departement'];?>
 </td>
                             <td><?php echo $_smarty_tpl->tpl_vars['ligne']->value['nom_type'];?>
