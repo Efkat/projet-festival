966920713cd8f03c3b17bce1fc09e3a382f1410f
NinoLS <belicnino77@gmail.com>
2021-01-06 12:07:33 +0100

    Add : dept,style,scene selection

--- templates/stats.tpl
+++ templates/stats.tpl
@@ -37,24 +37,49 @@
                     </div> *}
                     <div class="my-3 mx-5">
                         <legend>Voir par :</legend>
-                        <div>
+                        <div id="depts">
                             <input type="radio" name="viewBy" value="dept">
                             <label for="dept">Départements</label>
                         </div>
-                        <div>
+                        <div id="depts_div" class="my-3 mx-5" style="display:none;">
+                            <label for="departement-input">Département : </label>
+                            <select class="custom-select" name="departement" id="departement-input" required>
+                                <option value="0" selected>Tous</option>
+                                {foreach from=$depts item=dept}
+                                    <option value="{$dept['num_dept']}">{$dept['departement']}</option>
+                                {/foreach}
+                            </select>
+                        </div>
+                        <div id="styles">
                             <input type="radio" name="viewBy" value="style">
                             <label for="style">Styles</label>
                         </div>
-                        <div>
+                        <div id="styles_div" class="my-3 mx-5" style="display:none;">
+                            <label for="style-input">Style du groupe : </label>
+                            <select class="custom-select" name="style" id="style-input" required>
+                                {foreach from=$styles key=num item=nom}
+                                    <option value="{$num}">{$nom}</option>
+                                {/foreach}
+                            </select>
+                        </div>
+                        <div id="scenes">
                             <input type="radio" name="viewBy" value="scene">
                             <label for="scene">Scènes</label>
                         </div>
+                        <div id="scenes_div" class="my-3 mx-5" style="display:none;">
+                            <label for="scene-input">Scène du groupe : </label>
+                            <select class="custom-select" name="scene" id="scene-input" required>
+                                {foreach from=$scenes key=num item=nom}
+                                    <option value="{$num}">{$nom}</option>
+                                {/foreach}
+                            </select>
+                        </div>  
                     </div>
                     <div class="text-center">
                         <input type="submit" id="submit" value="Envoyer" class="btn btn-success btn-lg">
                     </div>
                 </form>
             </section>
-
         </main>
+        <script src="./script/selectDeptSceneStyle_stats.js"></script>
 {/block}
--- templates_c/b5494a6889f272f6fc7f27e9a7a3068186ed061e_0.file.stats.tpl.php
+++ templates_c/b5494a6889f272f6fc7f27e9a7a3068186ed061e_0.file.stats.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.36, created on 2021-01-05 23:06:55
+/* Smarty version 3.1.36, created on 2021-01-06 11:06:23
   from 'C:\laragon\www\projet-festival\templates\stats.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_5ff4f10fc55839_73982070',
+  'unifunc' => 'content_5ff599af5a1668_53694097',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'b5494a6889f272f6fc7f27e9a7a3068186ed061e' => 
     array (
       0 => 'C:\\laragon\\www\\projet-festival\\templates\\stats.tpl',
-      1 => 1609888014,
+      1 => 1609931179,
       2 => 'file',
     ),
   ),
@@ -20,26 +20,26 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ff4f10fc55839_73982070 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ff599af5a1668_53694097 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_4152174575ff4f10fc54376_28687207', "title");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_12462424105ff599af5928b4_43048081', "title");
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_3568858015ff4f10fc55071_08465567', "content");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_12139052935ff599af5935e3_83068714', "content");
 $_smarty_tpl->inheritance->endChild($_smarty_tpl, "base.tpl");
 }
 /* {block "title"} */
-class Block_4152174575ff4f10fc54376_28687207 extends Smarty_Internal_Block
+class Block_12462424105ff599af5928b4_43048081 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'title' => 
   array (
-    0 => 'Block_4152174575ff4f10fc54376_28687207',
+    0 => 'Block_12462424105ff599af5928b4_43048081',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -51,12 +51,12 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 }
 /* {/block "title"} */
 /* {block "content"} */
-class Block_3568858015ff4f10fc55071_08465567 extends Smarty_Internal_Block
+class Block_12139052935ff599af5935e3_83068714 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'content' => 
   array (
-    0 => 'Block_3568858015ff4f10fc55071_08465567',
+    0 => 'Block_12139052935ff599af5935e3_83068714',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -68,26 +68,80 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
                 <form action="./stats" method="POST">
                                         <div class="my-3 mx-5">
                         <legend>Voir par :</legend>
-                        <div>
+                        <div id="depts">
                             <input type="radio" name="viewBy" value="dept">
                             <label for="dept">Départements</label>
                         </div>
-                        <div>
+                        <div id="depts_div" class="my-3 mx-5" style="display:none;">
+                            <label for="departement-input">Département : </label>
+                            <select class="custom-select" name="departement" id="departement-input" required>
+                                <option value="0" selected>Tous</option>
+                                <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['depts']->value, 'dept');
+$_smarty_tpl->tpl_vars['dept']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['dept']->value) {
+$_smarty_tpl->tpl_vars['dept']->do_else = false;
+?>
+                                    <option value="<?php echo $_smarty_tpl->tpl_vars['dept']->value['num_dept'];?>
+"><?php echo $_smarty_tpl->tpl_vars['dept']->value['departement'];?>
+</option>
+                                <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+                            </select>
+                        </div>
+                        <div id="styles">
                             <input type="radio" name="viewBy" value="style">
                             <label for="style">Styles</label>
                         </div>
-                        <div>
+                        <div id="styles_div" class="my-3 mx-5" style="display:none;">
+                            <label for="style-input">Style du groupe : </label>
+                            <select class="custom-select" name="style" id="style-input" required>
+                                <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['styles']->value, 'nom', false, 'num');
+$_smarty_tpl->tpl_vars['nom']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['num']->value => $_smarty_tpl->tpl_vars['nom']->value) {
+$_smarty_tpl->tpl_vars['nom']->do_else = false;
+?>
+                                    <option value="<?php echo $_smarty_tpl->tpl_vars['num']->value;?>
+"><?php echo $_smarty_tpl->tpl_vars['nom']->value;?>
+</option>
+                                <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+                            </select>
+                        </div>
+                        <div id="scenes">
                             <input type="radio" name="viewBy" value="scene">
                             <label for="scene">Scènes</label>
                         </div>
+                        <div id="scenes_div" class="my-3 mx-5" style="display:none;">
+                            <label for="scene-input">Scène du groupe : </label>
+                            <select class="custom-select" name="scene" id="scene-input" required>
+                                <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['scenes']->value, 'nom', false, 'num');
+$_smarty_tpl->tpl_vars['nom']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['num']->value => $_smarty_tpl->tpl_vars['nom']->value) {
+$_smarty_tpl->tpl_vars['nom']->do_else = false;
+?>
+                                    <option value="<?php echo $_smarty_tpl->tpl_vars['num']->value;?>
+"><?php echo $_smarty_tpl->tpl_vars['nom']->value;?>
+</option>
+                                <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+                            </select>
+                        </div>  
                     </div>
                     <div class="text-center">
                         <input type="submit" id="submit" value="Envoyer" class="btn btn-success btn-lg">
                     </div>
                 </form>
             </section>
-
         </main>
+        <?php echo '<script'; ?>
+ src="./script/selectDeptSceneStyle_stats.js"><?php echo '</script'; ?>
+>
 <?php
 }
 }
